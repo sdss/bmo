ab5547a80ca982971d1d0f7df8433c71825d29a1
csayres <csayres@uw.edu>
2016-10-06 17:23:05 -0700

    working example with cv2

--- manta_example/mantaExample.py
+++ manta_example/mantaExample.py
@@ -1,6 +1,10 @@
 import pymba
 import numpy
 import matplotlib.pyplot as plt
+import traceback
+import sys
+import time
+import cv2
 
 if __name__ == "__main__":
     vimba = pymba.Vimba()
@@ -11,23 +15,36 @@ if __name__ == "__main__":
     camera0 = vimba.getCamera(cameraIds[0])
     camera0.openCamera()
     camera0.AcquisitionMode = "SingleFrame"
-    frame0 = camera0.getFrame()
-    frame0.announceFrame()
-    camera0.startCapture()
-    frame0.queueFrameCapture()
-
-    camera0.runFeatureCommand("AcquisitionStart")
-    camera0.runFeatureCommand("AcquisitionStop")
-    frame0.waitFrameCapture()
-    imgData = numpy.ndarray(buffer = frame0.getBufferByteData(),
+    tstart = time.time()
+    nImg = 0
+    while True:
+        try:
+            frame0 = camera0.getFrame()
+            frame0.announceFrame()
+            camera0.startCapture()
+            frame0.queueFrameCapture()
+            camera0.runFeatureCommand("AcquisitionStart")
+            camera0.runFeatureCommand("AcquisitionStop")
+            frame0.waitFrameCapture()
+            imgData = numpy.ndarray(buffer = frame0.getBufferByteData(),
                                    dtype = numpy.uint8,
                                    shape = (frame0.height,
                                             frame0.width)
                                             )
-    plt.imshow(imgData)
-    plt.show()
-
-    camera0.endCapture()
-    camera0.revokeAllFrames()
-    vimba.shutdown()
+            camera0.endCapture()
+            camera0.revokeAllFrames()
+            nImg += 1
+            print ("fps: %.4f, nimg: %i"%(nImg/float(time.time()-tstart), nImg))
+            #imgData = cv2.cvtColor(imgData, cv2.COLOR_BGR2GRAY)
+            cv2.imshow("frame", imgData)
+            cv2.waitKey(10)
+        except Exception as e:
+            print("oops")
+            print str(e)
+            traceback.print_exc(file=sys.stdout)
+            #camera0.endCapture()
+            cv2.destroyAllWindows()
+            vimba.shutdown()
+            break
+            #import pdb; pdb.set_trace()
 
