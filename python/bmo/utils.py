11e75a6d08152fdde9b6bc8a71960d191a83310c
José Sánchez-Gallego <gallegoj@uw.edu>
2017-01-07 02:14:06 -0300

    basic get_centroid function

---
+++ python/bmo/utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# utils.py
+#
+# Created by José Sánchez-Gallego on 6 Jan 2017.
+
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import PyGuide
+import numpy as np
+
+
+def get_centroid(image):
+
+    mask = np.zeros(image.shape)
+
+    ccdInfo = PyGuide.CCDInfo(np.median(image), 5, 5)
+    stars = PyGuide.findStars(image, mask, None, ccdInfo)
+
+    return stars
