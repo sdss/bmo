d653f4bac7c15d8c19c3f2b374bc1cac9ccf7ab2
José Sánchez-Gallego <gallegoj@uw.edu>
2017-03-22 06:00:47 -0300

    renamed show_in_ds9 and made it actor independent. Other minor changes.

--- python/bmo/utils.py
+++ python/bmo/utils.py
@@ -15,12 +15,11 @@ import numpy as np
 import os
 
 import astropy.table as table
+import pyds9
 
-import matplotlib.pyplot as plt
 
-
-focal_scale = 3600. / 330.275  # arcsec / mm
-pixel_size = 5.86 / 1000.  # in mm
+FOCAL_SCALE = 3600. / 330.275  # arcsec / mm
+PIXEL_SIZE = 5.86 / 1000.  # in mm
 
 
 def get_plateid(cartID):
@@ -51,6 +50,7 @@ def get_xyfocal_off_camera(plateID):
 
 
 def get_centroid(image):
+    """Uses PyGuide to return the brightest centroid in an array."""
 
     mask = np.zeros(image.shape)
 
@@ -63,73 +63,62 @@ def get_centroid(image):
     return centroids[0]
 
 
-def calculate_translation_offset(onaxis, offaxis, plot=False):
+def show_in_ds9(image, camera_type, ds9=None):
+    """Displays an image in DS9, calculating star centroids.
 
-    onaxis_centroid = get_centroid(onaxis)
-    offaxis_centroid = get_centroid(offaxis)
+    Parameters:
+        image (Numpy ndarray):
+            A Numpy ndarray containing the image to display.
+        camera_type ({'on_axis', 'off_axis'}):
+            The type of camera image being displayed.
+        ds9 (pyds9 object or None or str):
+            Either a ``pyds9`` object used to communicate with DS9, a string to
+            be used to create such a connection, or ``None``. In the latter
+            case ``pyds9.DS9`` will be called without arguments.
 
-    print('On-axis centroid: ', onaxis_centroid)
-    print()
-    print('Off-axis centroid: ', offaxis_centroid)
+    Returns:
+        result (None or tuple):
+            If no centroid has been found for the image, returns ``None``.
+            Otherwise, returns a tuple with the x, y position of the centroid,
+            and the radius, as detected by PyGuide.
 
-    if plot:
+    Example:
+        Opens a FITS file and displays it
+          >>> data = fits.getdata('image.fits')
+          >>> centroid = show_in_ds9(data, 'on_axis')
+          >>> print(centroid)
+          >>> (19.2, 145.1, 5.1)
 
-        fig, axes = plt.subplots(2, sharex=True)
-
-        axes[0].imshow(onaxis, origin='lower', cmap='gist_heat')
-        axes[0].scatter(onaxis_centroid.xyCtr[0], onaxis_centroid.xyCtr[1],
-                        s=50, marker='x', c='b')
-        axes[0].vlines(onaxis.shape[1] / 2, 0, onaxis.shape[0], colors=['y'], lw=1)
-        axes[0].hlines(onaxis.shape[0] / 2, 0, onaxis.shape[1], colors=['y'], lw=1)
-
-        axes[1].imshow(offaxis, origin='lower', cmap='gist_heat')
-        axes[1].scatter(offaxis_centroid.xyCtr[0], offaxis_centroid.xyCtr[1],
-                        s=50, marker='x', c='b')
-        axes[1].vlines(offaxis.shape[1] / 2, 0, offaxis.shape[0], colors=['y'], lw=1)
-        axes[1].hlines(offaxis.shape[0] / 2, 0, offaxis.shape[1], colors=['y'], lw=1)
-
-        plt.xlim(0, onaxis.shape[1])
-        plt.ylim(0, offaxis.shape[0])
-
-        plt.show()
-
-    centroids = np.array([onaxis_centroid.xyCtr, offaxis_centroid.xyCtr])
-    centre = np.array([onaxis.shape[::-1], offaxis.shape[::-1]]) / 2.
-
-    print()
-
-    trans = (centroids[0, :] - centre[0, :]) * pixel_size * focal_scale
-    trans[0] *= -1
-
-    print('Translation (Dec, RA): {0}'.format(trans))
-    # print('Rotation: {0:.1f}'.format(rot))
-    # print('Scale: {0:.3f}'.format(scale))
-
-
-def show_ds9(image, camera_type, ds9, actor):
+    """
 
     assert camera_type in ['on_axis', 'off_axis']
 
+    if not isinstance(ds9, pyds9.DS9):
+        if ds9 is None:
+            ds9 = pyds9.DS9()
+        elif isinstance(ds9, str):
+            ds9 = pyds9.DS9(ds9)
+        else:
+            raise ValueError('incorrect value for ds9 keyword: {0!r}'.format(ds9))
+
     try:
-        centroid = get_centroid(image.data)
+        centroid = get_centroid(image)
         xx, yy = centroid.xyCtr
-    except:
+        rad = centroid.rad
+    except AssertionError:
         centroid = None
 
-    if camera_type == 'on_axis':
-        frame = 1
-    else:
-        frame = 2
+    frame = 1 if camera_type == 'on_axis' else 2
 
     ds9.set('frame {0}'.format(frame))
-    ds9.set_np2arr(image.data)
+    ds9.set_np2arr(image)
     ds9.set('zoom to fit')
 
     ds9.set('regions command {{point({0}, {1}) # point=cross 20, color=blue}}'.format(
-        image.data.shape[1] / 2., image.data.shape[0] / 2.))
+        image.shape[1] / 2., image.shape[0] / 2.))
 
     if centroid:
-        ds9.set('regions command {{circle({0}, {1}, {2}) # color=green}}'.format(xx, yy,
-                                                                                 centroid.rad))
-        actor.writeToUsers('d', 'text="{0} camera centroid detected at ({1:.1f}, {2:.1f})"'
-                           .format(camera_type, xx, yy))
+        ds9.set('regions command {{circle({0}, {1}, {2}) # color=green}}'.format(xx, yy, rad))
+        return (xx, yy, rad)
+
+    return None
