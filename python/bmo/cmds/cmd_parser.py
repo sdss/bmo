d70a5f2e2c28559251b3fadc43331997de355cfd
José Sánchez-Gallego <gallegoj@uw.edu>
2017-09-27 02:52:14 -0700

    uncommenting commands

--- python/bmo/cmds/cmd_parser.py
+++ python/bmo/cmds/cmd_parser.py
@@ -12,14 +12,14 @@ from __future__ import absolute_import
 
 import click
 
-# from bmo.cmds.camera import camera
-# from bmo.cmds.centre_up import centre_up
-# from bmo.cmds.ds9 import ds9
-# from bmo.cmds.help import help
-# from bmo.cmds.ping import ping
-# from bmo.cmds.status import status
-# from bmo.cmds.tcc import tcc
-# from bmo.cmds.version import version
+from bmo.cmds.camera import camera
+from bmo.cmds.centre_up import centre_up
+from bmo.cmds.ds9 import ds9
+from bmo.cmds.help import help
+from bmo.cmds.ping import ping
+from bmo.cmds.status import status
+from bmo.cmds.tcc import tcc
+from bmo.cmds.version import version
 
 
 __all__ = ('bmo_parser')
@@ -31,14 +31,14 @@ def bmo_parser(ctx):
     pass
 
 
-# bmo_parser.add_command(camera)
-# bmo_parser.add_command(centre_up)
-# bmo_parser.add_command(ds9)
-# bmo_parser.add_command(help)
-# bmo_parser.add_command(ping)
-# bmo_parser.add_command(status)
-# bmo_parser.add_command(tcc)
-# bmo_parser.add_command(version)
+bmo_parser.add_command(camera)
+bmo_parser.add_command(centre_up)
+bmo_parser.add_command(ds9)
+bmo_parser.add_command(help)
+bmo_parser.add_command(ping)
+bmo_parser.add_command(status)
+bmo_parser.add_command(tcc)
+bmo_parser.add_command(version)
 
 
 if __name__ == '__main__':
