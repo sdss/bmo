538cae788ccf56c1df2c7248b0ba3dc88dc04d11
José Sánchez-Gallego <gallegoj@uw.edu>
2017-09-27 02:47:01 -0700

    and more

--- python/bmo/__init__.py
+++ python/bmo/__init__.py
@@ -14,18 +14,18 @@ import click
 import warnings
 
 
-def warning_on_one_line(message, category, filename, lineno, file=None, line=None):
-
-    basename = pathlib.Path(filename).name
-    category_colour = click.style('[{}]'.format(category.__name__), fg='yellow')
-
-    return '{}: {} ({}:{})\n'.format(category_colour, message, basename, lineno)
-
-
-warnings.formatwarning = warning_on_one_line
-
-warnings.filterwarnings(
-    'ignore', 'Matplotlib is building the font cache using fc-list. This may take a moment.')
+# def warning_on_one_line(message, category, filename, lineno, file=None, line=None):
+#
+#     basename = pathlib.Path(filename).name
+#     category_colour = click.style('[{}]'.format(category.__name__), fg='yellow')
+#
+#     return '{}: {} ({}:{})\n'.format(category_colour, message, basename, lineno)
+#
+#
+# warnings.formatwarning = warning_on_one_line
+#
+# warnings.filterwarnings(
+#     'ignore', 'Matplotlib is building the font cache using fc-list. This may take a moment.')
 
 
 # # Loads config
