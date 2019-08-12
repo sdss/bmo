968ab504cedcec017cc2e79504f6d6d502016afd
José Sánchez-Gallego <gallegoj@uw.edu>
2019-08-12 14:07:37 -0700

    Bump version: 0.2.5 → 0.2.6dev

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ install_requires = [line.strip().replace('==', '>=') for line in open(requiremen
                     if not line.strip().startswith('#') and line.strip() != '']
 
 NAME = 'bmo'
-VERSION = '0.2.5'
+VERSION = '0.2.6dev'
 
 
 def run(packages):
